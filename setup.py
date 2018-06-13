1aa5210277489066f4d2fd046a02a2e248de33e7
arcondello <arcondello@gmail.com>
2018-06-13 12:41:34 -0700

    Add requirements version ranges

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ setup(
     version='0.0.0',
     packages=['placeandroute','placeandroute.routing',
               'placeandroute.tilebased'],
-    install_requires=['networkx', 'dwave-networkx', 'six'],
+    install_requires=['networkx>=2.0,<3.0',
+                      'dwave-networkx>=0.6.3,<0.7.0',
+                      'six>=1.11.0,<2.0.0'
+                      ],
     url='https://bitbucket.org/StefanoVt/placeandroute/src/master/',
     license='MIT',
     author='Stefano Varotti',
