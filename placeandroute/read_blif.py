0417d1357dd06643745430805ea75993dde5552b
Stefano <stefano.varotti@unitn.it>
2019-02-10 20:28:39 +0100

    save wires map

--- placeandroute/read_blif.py
+++ placeandroute/read_blif.py
@@ -12,37 +12,54 @@ def chimera_map_constraint(pins, gate_data, ancilla_accum):
 
     #first half is top row, second half is bottom
     dim = gate_data["dims"][2]
-    allpos = range(1, dim*2+1)
+    allpos = list(range(1, dim*2+1))
     posmap = {x:0 for x in allpos[:dim]}
     posmap.update({y:1 for y in allpos[dim:]})
+    ordered_wires = []
+    ow = {}
 
     retg = [[], []]
     for pin, pos in zip(pins_names, gate_data["vP"][0][0]):
         pin = pin[0]
         assert isinstance(pin, int), ("error parsing genlib", pin)
         retg[posmap[pos]].append(pins[pin])
+        ow[pos] = pin
         del posmap[pos]
 
     #leftover posmap are ancilla
     for to in posmap.values():
-        retg[to] = "anc_" + str(ancilla_accum)
+        ancilla_name = "anc_" + str(ancilla_accum)
+        retg[to] = ancilla_name
+        ow[to] = ancilla_name
         ancilla_accum += 1
 
-    return Constraint(retg), ancilla_accum
+    for pos in range(1, dim*2+1):
+        ordered_wires.append(ow[pos])
 
-def read_blif(f, db, map_constraint):
-    # type: (TextIO, Dict, Callable) -> [Constraint]
+
+    return Constraint(retg), ordered_wires, ancilla_accum
+
+def read_blif(f):
+    # type: (TextIO) -> List[Tuple[str, Dict]]
     ret = []
-    ancilla_accum = 1
     for line in f:
         if not line.startswith(".gate"):
             continue
         gate_name, pins = blif_gate.match(line).groups()
         pins = {pin:var for pin, var in  blif_pins.findall(pins)}
+        ret.append((gate_name, pins))
+    return ret
+
+def blif_to_constraints(blif, db, map_constraint):
+    # type: (List[Tuple[str, Dict]], Dict, Callable) -> [Constraint]
+    ret = []
+    ancilla_accum = 1
+    for gate_name, pins in blif:
 
         gate_data = db[gate_name]
 
-        constr, ancilla_accum = map_constraint(pins, gate_data, ancilla_accum)
+        constr, wires, ancilla_accum = map_constraint(pins, gate_data, ancilla_accum)
+        constr.wires = wires
         ret.append(constr)
 
     return ret
